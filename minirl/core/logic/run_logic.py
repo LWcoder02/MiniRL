0d9902b0e980ed7fa5314e75a8424dbdca21e8fb
LWcoder02 <larsw02@gmail.com>
2024-06-20 09:14:15 +0200

    Added class for logic of training

---
+++ minirl/core/logic/run_logic.py
@@ -0,0 +1,21 @@
+
+
+class TrainLogic():
+    def __init__(self):
+        pass
+
+
+    def init_learn(self, num_steps_per_fit, num_episodes_per_fit, num_steps, num_episodes):
+        pass
+
+
+    def init_run(self, initial_state=None, verbose=True):
+        pass
+
+
+    def move_condition(self):
+        pass
+
+
+    def fit_condition(self):
+        pass
