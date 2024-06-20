43efc66f2d9f15525f53612dca8133fdb8942bec
LWcoder02 <larsw02@gmail.com>
2024-06-20 09:13:59 +0200

    Setup structure for core and agent

--- minirl/core/agent.py
+++ minirl/core/agent.py
@@ -1,5 +1,71 @@
+from minirl.core.serialization import Serialization
+from MiniRL.minirl.core.logic.run_logic import TrainLogic
 
 
-class Agent():
-    def __init__(self):
+class Agent(Serialization):
+    def __init__(self, policy):
+        self.policy = policy
+
+        self._logic = TrainLogic()
+
+
+    def learn(self, move_condition=None, fit_condition=None, verbose=True):
+        if move_condition is None:
+            move_condition = self._logic.move_condition
+        if fit_condition is None:
+            fit_condition = self._logic.fit_condition
+            
+        self._run(move_condition=move_condition,
+                  fit_condition=fit_condition,
+                  verbose=verbose)
+        
+
+    def _run(self, move_condition, fit_condition, init_state=None, verbose=True):
+        self._logic.init_run(init_state, verbose)
+
+        rollout = []
+
+        terminal = True
+        while move_condition():
+            if terminal:
+                self._reset(init_state)
+
+            sample = self.step()
+
+            if fit_condition():
+                self._train(rollout)
+
+
+                rollout.clear()
+
+            terminal = sample[5]
+
+        
+        self.stop()
+        # self.env.stop()
+
+        return rollout
+
+
+    def evaluate(self):
+        pass
+
+
+    def _train(self):
+        pass
+
+
+    def draw_action(self):
+        pass
+
+
+    def stop(self):
+        pass
+
+
+    def _reset(self):
+        pass
+
+
+    def step(self):
         pass
--- minirl/core/core.py
+++ minirl/core/core.py
@@ -1,5 +1,28 @@
+from MiniRL.minirl.core.logic.run_logic import TrainLogic
+from minirl.core.agent import Agent
 
 
 class Core():
-    def __init__(self):
+    def __init__(self, agent: Agent, environment):
+        self.agent = agent
+        self.environment = environment
+
+        self._logic = TrainLogic()
+
+
+    def learn(self, num_steps=None, num_episodes=None, num_steps_per_fit=None, num_episodes_per_fit=None,
+              verbose=True):
+        self._logic.init_learn(num_steps_per_fit, num_episodes_per_fit, num_steps, num_episodes)
+
+
+        dataset = self.agent.learn(verbose)
+        return dataset
+
+
+    def _reset(self, initial_state=None):
+        pass
+
+
+
+    def _step(self):
         pass
