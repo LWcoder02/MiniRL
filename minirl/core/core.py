5628fe039ca552e0938bc551c31a75e0a8bbe54b
LWcoder02 <larsw02@gmail.com>
2024-06-21 07:34:55 +0200

    Combined learn and evaluation method

--- minirl/core/core.py
+++ minirl/core/core.py
@@ -10,24 +10,29 @@ class Core():
         self._logic = TrainLogic()
 
 
-    def learn(self, num_steps=None, num_episodes=None, num_steps_per_fit=None, num_episodes_per_fit=None,
-              verbose=True):
-        self._logic.init_learn(num_steps_per_fit, num_episodes_per_fit, num_steps, num_episodes)
+    def train(self, config, num_steps=None, num_steps_fit=None,
+              num_episodes=None, num_episodes_fit=None, verbose=True):
+        
 
+        scores = {
+            'J': []
+        }
+        # Start the training
+        for epoch in config['epochs']:
+            self.agent.update_policy() # i.e. set epsilon
+            print(f"Beginn learning of epoch {epoch+1}")
+            self._logic.init_learn(num_steps_fit, num_episodes_fit, num_steps, num_episodes)
+            self.agent.learn(environment=self.environment, verbose=verbose)
 
-        dataset = self.agent.learn(environment=self.environment,
-                                   verbose=verbose)
-        return dataset
-
-
-    def evaluate(self, num_steps=None, num_episodes=None, initial_state=None, verbose=True):
-
-        self._logic.init_evaluate(num_steps, num_episodes)
-
-        dataset = self.agent.evaluate(initial_state=initial_state, verbose=verbose)
-        return dataset
+            print(f"Beginn evaluation of epoch {epoch+1}")
+            self._logic.init_evaluate(num_steps, num_episodes)
+            dataset = self.agent.evaluate(verbose=verbose)
+            scores['J'].append(self.compute_J(dataset))
 
 
     def _reset(self, initial_state=None):
         pass
 
+
+    def compute_J(self, dataset):
+        pass
