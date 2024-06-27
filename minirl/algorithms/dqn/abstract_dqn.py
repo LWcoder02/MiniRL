9c10f6c058f625a7f61089a7ec972883bff82b37
LWcoder02 <larsw02@gmail.com>
2024-06-27 11:26:05 +0200

    Added DQN structure

---
+++ minirl/algorithms/dqn/abstract_dqn.py
@@ -0,0 +1,40 @@
+from copy import deepcopy
+
+import numpy as np
+import torch
+
+from minirl.core.agent import Agent
+from minirl.approximators.torch_approximators import TorchApproximator
+
+
+class AbstractDQN(Agent):
+    def __init__(self):
+        
+        self._replay_buffer = []
+        self._fit = self._train_standard
+
+        self.approximator = TorchApproximator
+        self.target_approximator = TorchApproximator
+
+
+    def _train(self, dataset):
+        self._fit(dataset)
+
+
+    def _train_standard(self, dataset):
+        self._replay_buffer.add(dataset)
+        if self._replay_buffer.init:
+            state, action, reward, next_state, terminal, info = self._replay_buffer.sample()
+
+            # clip reward here
+
+            q_next = self._q_next(next_state, terminal)
+            q = reward + self.mdp_info.gamma * q_next
+
+
+            pred = torch.gather(input=state, index=action)
+            self.approximator.fit(pred, q)
+
+
+    def _q_next(self, next_state, terminal):
+        raise NotImplementedError()
---
+++ minirl/algorithms/dqn/dqn.py
@@ -0,0 +1,10 @@
+from minirl.algorithms.dqn.abstract_dqn import AbstractDQN
+
+
+class DQN(AbstractDQN):
+    def _q_next(self, next_state, terminal):
+        q = self.target_approximator.predict(next_state)
+        if terminal.any():
+            q *= 1 - terminal.reshape(-1, 1)
+
+        return q.max(1)
