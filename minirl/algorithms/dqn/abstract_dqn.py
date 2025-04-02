bf730e5385c6617c61ffc9e4ba5b0726c81266bd
LWcoder02 <larsw02@gmail.com>
2025-04-02 10:11:03 +0200

    implemented abstract dqn

--- minirl/algorithms/dqn/abstract_dqn.py
+++ minirl/algorithms/dqn/abstract_dqn.py
@@ -1,7 +1,6 @@
 from copy import deepcopy
 
 import numpy as np
-import torch
 
 from minirl.core.agent import Agent
 from minirl.approximators.torch_approximators import TorchApproximator
@@ -9,25 +8,42 @@ from minirl.rl_utils.replay_memory import ReplayBuffer
 
 
 class AbstractDQN(Agent):
-    def __init__(self):
+    def __init__(self, environment, policy, approximator_params: dict, approximator: TorchApproximator = TorchApproximator, batch_size=32, target_update_frequency=50,
+                 initial_replay_size=500, max_replay_size=5000, clip_reward=False):
         
         self._replay_buffer = ReplayBuffer()
         self._fit = self._train_standard
 
-        self.approximator = TorchApproximator
-        self.target_approximator = TorchApproximator
+
+        approximator_params = deepcopy(approximator_params)
+        approximator_params_target = deepcopy(approximator_params)
+        self._initialize_approximator(approximator, approximator_params, approximator_params_target)
+
+        self._num_updates = 0
+        self._clip_reward = clip_reward
+        self._update_frequency = target_update_frequency
+        self._batch_size = batch_size
+
+        policy.set_approximator(self.approximator)
+        super().__init__(environment=environment, policy=policy)
 
 
     def _train(self, dataset):
         self._fit(dataset)
 
+        self._num_updates += 1
+        if self._num_updates % self._update_frequency == 0:
+            self._update_target()
+
 
     def _train_standard(self, dataset):
         self._replay_buffer.add(dataset)
         if self._replay_buffer.init:
-            state, action, reward, next_state, terminal, info = self._replay_buffer.sample()
+            state, action, reward, next_state, terminal, info = self._replay_buffer.sample(self._batch_size)
 
             # clip reward here
+            if self._clip_reward:
+                reward = np.clip(reward, -1, 1)
 
             q_next = self._q_next(next_state, terminal)
             q = reward + self.env_info.gamma * q_next
@@ -39,3 +55,13 @@ class AbstractDQN(Agent):
 
     def _q_next(self, next_state, terminal):
         raise NotImplementedError()
+    
+
+    def _initialize_approximator(self, approximator, approximator_params, approx_params_target):
+        self.approximator: TorchApproximator = approximator(approximator_params)
+        self.target_approximator: TorchApproximator = approximator(approx_params_target)
+        self._update_target()
+
+
+    def _update_target(self):
+        self.target_approximator.set_weights(self.approximator.get_weights())
